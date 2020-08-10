126f67b399f52c83a51845c5cb90c2b858ea021b
root <dawid.b.tracz@gmail.com>
2020-08-10 19:45:45 +0200

    add dependencies installation

--- net/install.sh
+++ net/install.sh
@@ -1,13 +1,16 @@
 
-# apt-get update
+apt-get update
+apt-get install -y wget unzip cmake libopencv-dev
+ln -s /usr/include/opencv4/opencv2 /usr/include/opencv2
 
 LIBTORCH="libtorch-shared-with-deps-1.6.0"
 wget "https://download.pytorch.org/libtorch/cpu/$LIBTORCH%2Bcpu.zip"
 unzip "$LIBTORCH+cpu.zip"
+rm "$LIBTORCH+cpu.zip"
 
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH=/workspace/libtorch -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_PREFIX_PATH=$PWD/../libtorch -DCMAKE_BUILD_TYPE=Release ..
 make
 cd ..
 

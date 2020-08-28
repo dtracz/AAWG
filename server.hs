eee148370582ac45d0c69eee9f7170b585aa68b7
dawid <dawid.b.tracz@gmail.com>
2020-08-28 20:54:54 +0200

    receive, decode base64 and save image

--- server.hs
+++ server.hs
@@ -6,21 +6,36 @@ import Network.HTTP.Types (status200)
 import Blaze.ByteString.Builder (copyByteString)
 import qualified Data.ByteString.UTF8 as BU
 import Data.Monoid
+import Data.ByteString.Base64
 import Data.Text.Internal
- 
+
+import Codec.Picture
+
 main = do
     let port = 3000
     putStrLn $ "Listening on port " ++ show port
     run port app
  
 app req respond = do
-    -- let reqStr = showText $ head (pathInfo req)
-    iobs <- getRequestBodyChunk req
-    let body = BU.toString iobs
-    putStrLn $ ">>" ++ body ++ "<<"
-    res <- respond $ respConst "OK"
+    bs <- getRequestBodyChunk req
+    res <- getImage bs
+    let body = BU.toString bs
+    putStrLn $ ">>" ++ res ++ "<<"
+    res <- respond $ respConst res
     return res
- 
+
+getImage :: BU.ByteString -> IO String
+getImage bs = do
+    let bs' = decodeLenient bs
+    case decodeImage bs' of
+        Left msg -> return msg 
+        Right img -> saveImage img
+
+saveImage :: DynamicImage -> IO String
+saveImage img = do
+    savePngImage "test.png" img
+    return "OK"
+
 
 -- bsbConst :: Show a => a -> Data.ByteString.Builder.Internal.Builder
 bsbConst x = mconcat $ map copyByteString [ BU.fromString $ show x ]

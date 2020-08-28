95c88f1e60c2470a8924f284f1e19faded905d81
dawid <dawid.b.tracz@gmail.com>
2020-08-28 06:13:23 +0200

    simple server action on request

--- server.hs
+++ server.hs
@@ -6,22 +6,26 @@ import Network.HTTP.Types (status200)
 import Blaze.ByteString.Builder (copyByteString)
 import qualified Data.ByteString.UTF8 as BU
 import Data.Monoid
+import Data.Text.Internal
  
 main = do
     let port = 3000
     putStrLn $ "Listening on port " ++ show port
     run port app
  
-app req respond = respond $
-    case pathInfo req of
-        ["yay"] -> yay
-        x -> index x
+app req respond =
+    do
+        res <- respond $ reaction $ action (showText $ head (pathInfo req))
+        putStrLn $ "!!!!!!!!"
+        return res
  
-yay = responseBuilder status200 [ ("Content-Type", "text/plain") ] $ mconcat $ map copyByteString
-    [ "yay" ]
- 
-index x = responseBuilder status200 [("Content-Type", "text/html")] $ mconcat $ map copyByteString
-    [ "<p>Hello from ", BU.fromString $ show x, "!</p>"
-    , "<p><a href='/yay'>yay</a></p>\n" ]
+action :: String -> String
+action str = ">" ++ str ++ "<" 
+
+
+getAns x = mconcat $ map copyByteString [ BU.fromString $ show x ]
+
+reaction :: Show a => a -> Response
+reaction x = responseBuilder status200 [("Content-Type", "text/html")] $ getAns x
 
 
